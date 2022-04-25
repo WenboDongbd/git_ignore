1a8197ff33e749e5e8eb8ab8907bc38a185a8e7e
WenboDongbd <77481334+WenboDongbd@users.noreply.github.com>
2022-04-25 15:25:36 +0800

    git界面操作试一次pull

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -2,5 +2,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git 提交");
         System.out.println("再试一次");
+        System.out.println("git界面再试一次");
     }
 }
